7c72bf0ab6845f53ba1ebf5a622b5605af54a25f
Mattea29 <98430244+Mattea29@users.noreply.github.com>
2023-10-23 20:52:56 -0400

    adding necessary methods to House

--- House.java
+++ House.java
@@ -1,12 +1,44 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
+public class House extends Building {
 
-  public House() {
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = new ArrayList<>();
     System.out.println("You have built a house: 🏠");
   }
 
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+
+  public int nResidents(){
+    return residents.size()
+  }
+
+  public void moveIn(String name) {
+    residents.add(name);
+  }
+
+  public String moveOut(String name) {
+    if (residents.remove(name)) {
+      return name;
+    } else {
+      return null;
+    }
+  }
+
+  public boolean isResident(String person) {
+    return residents.contains(person);
+  }
+
+  // Testing
   public static void main(String[] args) {
-    new House();
+    new House("Comstock", "1 Mandelle Road", 3, true);
   }
 
 }
